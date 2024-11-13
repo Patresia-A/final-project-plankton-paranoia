2ceb0774a4b03f02d62a262d50554571746f0ac7
Hannah Bruch <hannahbruch03@gmail.com>
2024-11-13 01:39:52 -0700

    add translation from japanese to webscraper

--- scraper/main.js
+++ scraper/main.js
@@ -1,9 +1,14 @@
-// scrape from all these pages https://remywiki.com/Category:DanceDanceRevolution_Songs
 
 const axios = require("axios");
 const cheerio = require("cheerio");
 const chronoGames = require("./chronologically-sorted-games")
+const hepburn = require("hepburn")
+const wanakana = require('wanakana');
 
+const oldRatedGames = chronoGames.splice(0, 15)
+const newRatedGames = chronoGames.splice(-10)
+
+// scrape from all these pages https://remywiki.com/Category:DanceDanceRevolution_Songs
 function parseRuntime(time) {
     const arr = time.split(":");
     return parseInt(arr[1]) * 60 + parseInt(arr[2]);
@@ -14,16 +19,16 @@ function parseBPM(bpm) {
         return {
             lower: arr[0],
             //we do this split nonsense because sometimes the BPM might have more info after the number, for an example see https://remywiki.com/MAX_360 
-            higher: arr[1].split(" ")[0],  
+            higher: arr[1].split(" ")[0],
         };
     } else {
         return {
             lower: arr[0],
-            higher: arr[0].split(" ")[0], 
+            higher: arr[0].split(" ")[0],
         };
     }
 }
-async function fetch_page(page) {
+async function parse_page(page) {
     try {
         const response = await axios.get(page);
         const html = response.data;
@@ -42,7 +47,13 @@ async function fetch_page(page) {
                 song.higherBPM = parseInt(bpmObj.higher);
                 song.changingBPM = bpmObj.lower != bpmObj.higher
             }
-            else if (element.includes("Artist")) song.artist = element.split(':').slice(1).join(':').trim();
+            else if (element.includes("Artist")) {
+                let artist = element.split(':').slice(1).join(':').trim();
+                //handling japanese words
+                if (hepburn.containsKana(artist))
+                    artist = wanakana.toRomaji(artist)
+                song.artist = artist
+            }
 
             else if (element.includes("Length")) song.runtime = parseRuntime(element);
 
@@ -76,6 +87,9 @@ async function fetch_page(page) {
                 song.licensed = true;
             }
         });
+        if (song.playable) {
+            song.isNewRated = true;
+        }
 
         //difficulty class meaning "expert", "challenge", "beginner"
         const difficultyClasses = [];
@@ -87,6 +101,9 @@ async function fetch_page(page) {
             .each((i, cell) => {
                 difficultyClasses.push($(cell).text().trim());
             });
+        
+
+
         // the rows of raw data containing difficulties and notecounts
         const difficultyRows = []
         $('h3:has(.mw-headline#DanceDanceRevolution)')
@@ -95,32 +112,88 @@ async function fetch_page(page) {
             .each((i, row) => {
                 const rowData = [];
                 $(row).find('td').each((j, cell) => {
+                    // console.log(cell).text().trim()
                     rowData.push($(cell).text().trim());
                 });
                 if (rowData.length != 0)
                     difficultyRows.push(rowData);
             });
-        
+    // case like in https://remywiki.com/Acid,Tribal_%26_Dance_(DDR_EDITION), where it just says difficulty and notecounts
+        if (difficultyClasses.length === 0){
+            $('h2:has(.mw-headline)')
+            .next('table')
+            .find('tbody tr')
+            .eq(1)
+            .find('th')
+            .each((i, cell) => {
+                difficultyClasses.push($(cell).text().trim());
+            });
+            $('h2:has(.mw-headline)')
+            .next('table')
+            .find('tbody tr')
+            .each((i, row) => {
+                const rowData = [];
+                $(row).find('td').each((j, cell) => {
+                    console.log$(cell).text().trim()
+                    rowData.push($(cell).text().trim());
+                });
+                if (rowData.length != 0)
+                    difficultyRows.push(rowData);
+            });
+        }
+
         charts = []
+
+        let currentRatingRowIndex = 0;
+        song.isNewRated = false;
+        //setting mostRecentGame to the first ddr game
+
+        //determining most recent  game rating
+        for (let i = 1; i < difficultyRows.length; i++) {
+            const gameName = difficultyRows[i][0]
+            if (gameName.includes("Present")) {
+                currentRatingRowIndex = i;
+                song.isNewRated = true;
+                break;
+            }
+        }
+        //case where present is not found
+        //I can't find a great way to determine the most recent ratings for a song which has been removed.... I will just suffer
+        if (currentRatingRowIndex === 0) {
+            currentRatingRowIndex = 1
+        }
+
+        //determining newrating status
+        // if (!isNewRated){
+
+        //     for (let i = 1; i < difficultyRows.length; i++) {
+        //         if (!isNewRated) {
+        //             for (let j = 0; j < newRatedGames.length; j++) {
+        //                 if (newRatedGames[j].includes(difficultyRows[i][0])) {
+        //                     song.isNewRated = true;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
         let isDoubles = false;
-        let foundCurrentRating = false;
-        let currentRatingIndex = 0;
+
+        console.log(difficultyClasses)
         for (let i = 0; i < difficultyClasses.length; i++) {
             //parsing if chart is a doubles chart...
-            if (isDoubles || i > 5 || ((difficultyClasses[i] === "Beginner" || difficultyClasses[i] === "Basic" || difficultyClasses[i]=== "Difficult") && 
-                (difficultyClasses[i - 1] === "Expert" || difficultyClasses[i - 1] === "Challenge"))){
-                    isDoubles = true;
+            if (isDoubles || i > 5 || ((difficultyClasses[i] === "Beginner" || difficultyClasses[i] === "Basic" || difficultyClasses[i] === "Difficult") &&
+                (difficultyClasses[i - 1] === "Expert" || difficultyClasses[i - 1] === "Challenge"))) {
+                isDoubles = true;
             }
 
-            const noteArr = difficultyRows[0][i+1].split("/")
-            
+            const noteArr = difficultyRows[0][i + 1].split("/")
+
             //Notecount might be split in two ways... either "680 / 4" or "948 / 9 / 0".. thus we must handle shocks differently
             let shockNotes = 0;
-            if (difficultyRows.length === 2){
+            if (difficultyRows.length === 2) {
                 shockNotes = parseInt(noteArr[2])
             }
-            //finding current rating
-            
+            console.log("gHELLO")
             charts.push(
                 {
                     "difficulty": difficultyClasses[i],
@@ -128,7 +201,7 @@ async function fetch_page(page) {
                     "notes": parseInt(noteArr[0]),
                     "freezeNotes": parseInt(noteArr[1]),
                     "shockNotes": shockNotes,
-                    "difficultyRating": ""
+                    "difficultyRating": parseInt((difficultyRows[currentRatingRowIndex][i + 1]).replace("↑", "").replace("↓",""))
                 }
             )
         }
@@ -141,4 +214,4 @@ async function fetch_page(page) {
     }
 }
 
-fetch_page("https://remywiki.com/Scarlet_keisatsu_no_ghetto_patrol_24_ji");
+parse_page("https://remywiki.com/Scarlet_keisatsu_no_ghetto_patrol_24_ji");
